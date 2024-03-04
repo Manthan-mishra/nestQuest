c8bdf64d56f31ffdb101777bf4383e56494fc1d3
Manthan-mishra <manthan9481147615@gmail.com>
2024-03-04 17:22:52 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOOSE_URI)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(`error occured: ${err}`);
+  });
 const app = express();
 
 app.listen(8000, () => {
