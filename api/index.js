e1021bd2d3584111d6961f58691ef0ce3157b11c
Manthan-mishra <manthan9481147615@gmail.com>
2024-03-04 18:05:57 +0530

    separation of routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -17,3 +18,5 @@ const app = express();
 app.listen(8000, () => {
   console.log("server listening at port number 8000");
 });
+
+app.use("/api/user", userRouter);
