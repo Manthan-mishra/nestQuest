50b48261980621a9b71ccbf1a816715a749c6087
Manthan-mishra <manthan9481147615@gmail.com>
2024-03-11 10:20:46 +0530

    checking username and password exist in db

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,8 +16,13 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+  const validUser = await User.findOne({ email });
+  // console.log("valid user ", validUser);
+  if (!validUser) return next(errorHandler(404, "User not found"));
+  const validPassword = bcryptjs.compareSync(password, validUser.password);
+  if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
   try {
   } catch (err) {
-    next;
+    next(err);
   }
 };
