ed2a589213602f5884a7d0b25d6d8d5688f22dcd
Manthan-mishra <manthan9481147615@gmail.com>
2024-03-12 22:06:56 +0530

    Creation of sign in  api route complete

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 import express from "express";
 
 const userAuth = express.Router();
 
 userAuth.post("/signup", signup);
+userAuth.post("/signin", signin);
 
 export default userAuth;
