eef7588913ed2bd68a77bef9995ef14c31892d73
Manthan-mishra <manthan9481147615@gmail.com>
2024-05-22 13:25:57 +0530

    delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 
 const Profile = () => {
@@ -75,8 +78,19 @@ const Profile = () => {
         setUpdateSuccess(true);
       })
       .catch((error) => {
-        console.log("error ", error);
         dispatch(updateUserFailure(error.response.data.message));
+      });
+  };
+
+  const handleUserDelete = async (e) => {
+    dispatch(deleteUserStart());
+    await axios
+      .delete(`/api/user/delete/${currentUser._id}`)
+      .then((res) => {
+        dispatch(deleteUserSuccess(res.data));
+      })
+      .catch((error) => {
+        dispatch(deleteUserFailure(error.response.data.message));
       });
   };
 
@@ -139,7 +153,12 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleUserDelete}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
