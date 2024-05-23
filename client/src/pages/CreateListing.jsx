5f15cbc590032b364f9401281c0ce2417ef84420
Manthan-mishra <manthan9481147615@gmail.com>
2024-05-23 23:49:54 +0530

    upload listing Images function for create listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    // name: "",
+    // description: "",
+    // address: "",
+    // type: "rent",
+    // bedrooms: 1,
+    // bathrooms: 1,
+    // regularPrice: 50,
+    // discountPrice: 0,
+    // offer: false,
+    // parking: false,
+    // furnished: false,
+  });
+
+  console.log("form data ", formData);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.concat(urls),
+        });
+      });
+    }
+  };
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -115,6 +173,7 @@ const CreateListing = () => {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
@@ -122,6 +181,7 @@ const CreateListing = () => {
               multiple
             />
             <button
+              onClick={handleImageSubmit}
               type="button"
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:bg-green-700 hover:text-white hover:shadow-lg"
             >
